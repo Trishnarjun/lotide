1d74258a9f7f4e449ce0c5dc45cb9120df5e7b6c
Trishnarjun Kunamalla <trishnarjun.k@gmail.com>
2022-03-09 15:11:56 +0000

    fixed the if statment for asssetEqual function to the modular version

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,6 @@
 const assertEqual = function(actual, expected) {
-  if (actual === true && expected === true) {
+  console.log(actual,expected)
+  if (actual == expected) {
   console.log(`😏😏😏 Assertion Passed: ${actual} === ${expected}`);
   } else {
   console.log(`🤧🤧🤧 Assertion Failed: ${actual} !==  ${expected}`);
@@ -19,7 +20,7 @@ const eqArrays = function(arrayOne, arrayTwo) {
   } 
 };
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => true
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => true
