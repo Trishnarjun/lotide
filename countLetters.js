4cca3ad4bd29366859b6c3e6f77f75c7378621e4
Trishnarjun Kunamalla <trishnarjun.k@gmail.com>
2022-03-10 01:01:56 +0000

    counting letters

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😏😏😏 Assertion Passed: ${actual} === ${expected}`);
+    } else {
+    console.log(`🤧🤧🤧 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const countLetters = function(arr) {
+  let objects = {};
+  for (let i = 0; i < arr.length; i++) {
+    let counter = 0;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr.charAt(i) === arr.charAt(j)) {
+        counter++
+        let key = arr.charAt(i);
+        objects[key] = counter;
+      }
+    } 
+  }
+  console.log(objects);
+  // for (let k = 0; i < array.length; k++) {
+    
+  // }
+}
+
+countLetters('LHL');
+countLetters("lighthouse in the house")
