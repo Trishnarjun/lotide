49aa1a3aa880fdd09edd5c12c285efc946e7db46
Trishnarjun Kunamalla <trishnarjun.k@gmail.com>
2022-03-11 02:24:21 +0000

    slicing array till the cutoff specified

---
+++ takeUntil.js
@@ -0,0 +1,47 @@
+const takeUntil = function(array, callback) {
+  let result = []; 
+  const cutOff = array.filter(callback);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === cutOff[0]) {
+      return result;
+    } else {
+      result.push(array[i]);
+    }
+  };
+  return result
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let counter = 0;
+  for (let i = 0; i < arrayOne.length; i++ ) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      counter++;
+    }
+  }
+  if (counter === arrayOne.length & counter === arrayTwo.length ){
+    return true
+  } else{
+    return false;
+  } 
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne,arrayTwo) === true) {
+    console.log(`😏😏😏 Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🤧🤧🤧 Assertion Failed: ${arrayOne} !==  ${arrayTwo}`);
+  }
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2,[ "I've", 'been', 'to', 'Hollywood' ]);
